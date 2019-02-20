7f16571e5263b50b82c509f7eb66e8e332aa60ae
Jadwiga Bochinska <matuzalema@gmail.com>
2019-02-20 13:57:07 +0100

    Initialize project

--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,19 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'images/man-user.png'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
-        )
-      )
+      <div className={'contactItem'}>
+        <img className={'contactImage'} src={'images/man-user.png'}/>
+        <p className={'contactLabel'}> 
+          Imię: {this.props.item.firstName}
+        </p>
+        <p className={'contactLabel'}> 
+          Nazwisko: {this.props.item.lastName}
+        </p>
+        <a className={'contactEmail'} href='mailto:'>
+          {this.props.item.email}
+        </a>
+          {this.props.item.email}
+      </div>
     );
   },
 });
--- components/Contacts.js
+++ components/Contacts.js
@@ -9,7 +9,7 @@ var Contacts = React.createClass({
     });
 
     return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
+      <ul className={'contactsList'}>{contacts}</ul>
     );
   }
 });
